6668f7c7caa8d0ef731b65de68102b8b15469dac
artur <killpayne1@gmail.com>
2025-03-17 03:06:10 +0200

    added withCredentials

--- shorter.client/ClientApp/src/app/api.service.ts
+++ shorter.client/ClientApp/src/app/api.service.ts
@@ -1,22 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment';
+import { Shortlink } from './shortlinks/shortlinks.component'; // Import the interface
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  // Updated API URL with 'api' segment:
   private apiUrl: string = 'https://localhost:7161/api/Url';
 
   constructor(private http: HttpClient) { }
 
   getUrls(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    return this.http.get(this.apiUrl, { withCredentials: true });
   }
 
-  createUrl(url: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/CreateUrl`, url);
+  deleteUrl(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`, { withCredentials: true });
   }
 }
